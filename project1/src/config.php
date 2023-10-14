e4f714f56030d9763038f59878856975268dab92
avahak <a.vahakangas.work@gmail.com>
2023-10-15 01:34:55 +0300

    better debug div

--- project1/src/config.php
+++ project1/src/config.php
@@ -1,37 +1,66 @@
 <?php
 
 // Sets up environment variables into $GLOBALS["config"].
+function setup_globals_config() {
+    $is_localhost = in_array($_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1'));
+    $is_azure = (strpos($_SERVER['HTTP_HOST'], 'azurewebsites') !== false);
 
-$is_localhost = in_array($_SERVER['REMOTE_ADDR'], array('127.0.0.1', '::1'));
-$is_azure = (strpos($_SERVER['HTTP_HOST'], 'azurewebsites') !== false);
+    $config = new stdClass();
 
-$config = new stdClass();
-
-// localhost settings:
-if ($is_localhost) {
-    $config_file = "d:/projects/project1/localhost_settings.json";
-    if (!file_exists($config_file))
-        $config_file = "c:/projects/project1/localhost_settings.json";
-    if (!file_exists($config_file))
-        exit("Error! Missing localhost configuration file.");
-    $config = json_decode(file_get_contents($config_file), true);
-    if (!$config)
-        exit("Error decoding localhost configuration file.");
-}
-
-// AZURE settings:
-if ($is_azure) {
-    $config = [];
-    foreach ($_ENV as $key => $value) {
-        if (strpos($key, "APPSETTING_") === 0) 
-            $config[substr($key, 11)] = $value;
+    // localhost settings:
+    if ($is_localhost) {
+        $config_file = "d:/projects/project1/localhost_settings.json";
+        if (!file_exists($config_file))
+            $config_file = "c:/projects/project1/localhost_settings.json";
+        if (!file_exists($config_file))
+            exit("Error! Missing localhost configuration file.");
+        $config = json_decode(file_get_contents($config_file), true);
+        if (!$config)
+            exit("Error decoding localhost configuration file.");
     }
-    if (empty($config))
-        exit("Error reading AZURE environment variables.");
+
+    // AZURE settings:
+    if ($is_azure) {
+        $config = [];
+        foreach ($_ENV as $key => $value) {
+            if (strpos($key, "APPSETTING_") === 0) 
+                $config[substr($key, 11)] = $value;
+        }
+        if (empty($config))
+            exit("Error reading AZURE environment variables.");
+    }
+
+    $GLOBALS["CONFIG"] = $config;
 }
 
+// Set up error message logging
+function setup_error_handling($error_log_file) {
+    error_reporting(E_ALL & ~E_NOTICE);
+    // error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+    ini_set('log_errors', 1);
+    ini_set('error_log', $error_log_file); // Replace with your log file path
 
-$GLOBALS["CONFIG"] = $config;
+    $max_file_size = 2 * 1024 * 1024; // 2MB
+    if (file_exists($error_log_file) && filesize($error_log_file) > $max_file_size) {
+        // If the log file is too large, trim it
+        $log_data = file_get_contents($log_file);
+        $log_data = substr($log_data, -ceil(0.1*$max_file_size)); // leave 10% of data
+        file_put_contents($log_file, $log_data);
+    }
+
+    register_shutdown_function(function() {
+        $error = error_get_last();
+        if ($error !== null && $error['type'] === E_ERROR)
+            error_log("Fatal error: " . $error['message'] . " in " . $error['file'] . " on line " . $error['line']);
+    });
+}
+
+setup_globals_config();
+setup_error_handling(__DIR__ . "/../../logs/php_errors.log");
+// setup_error_handling("c:/xampp/htdocs/kurssi/logs/php_errors.log");
+
+// trigger_error("YYYYY", E_USER_ERROR);
 
 // Compare the following with Python's 'if __name__ == "__main__"':
 $is_direct_request = str_ends_with(str_replace('\\', '/', __FILE__), $_SERVER['SCRIPT_NAME']);
--- project1/src/debug_element.php
+++ project1/src/debug_element.php
@@ -7,21 +7,24 @@ function include_debug_div() {
     $GLOBALS["debug"] = ["how to use this?", "we already have g_logger"];
     ob_start(); ?>
 
-    <div id="debug-div" class="d-block m-3" style="position:fixed;bottom:0;">
-        <nav class="navbar navbar-dark bg-dark p-1">
+    <div id="debug-div" class="d-block m-3" style="position:fixed;bottom:0px;">
+        <nav id="debug-div-nav" class="navbar navbar-dark bg-dark p-1">
             <ul class="nav nav-tabs navbar-dark bg-dark collapse debug-collapse" role="tablist">
                 <li class="nav-item">
                     <button type="button" class="nav-link active" id="button-general" data-bs-toggle="tab" data-bs-target="#general" role="tab">General</button>
                 </li>
                 <li class="nav-item">
-                    <button type="button" class="nav-link" id="button-debug" data-bs-toggle="tab" data-bs-target="#debug" role="tab">Debug</button>
+                    <button type="button" class="nav-link" id="button-php-errors" data-bs-toggle="tab" data-bs-target="#php-errors" role="tab">PHP Errors</button>
+                </li>
+                <li class="nav-item">
+                    <button type="button" class="nav-link" id="button-logger" data-bs-toggle="tab" data-bs-target="#logger" role="tab">Logger</button>
                 </li>
             </ul>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target=".debug-collapse" style="outline:none;box-shadow:none;">
                 <span class="navbar-toggler-icon"></span>
             </button>
         </nav>
-        <div id="debug_content" class="collapse debug-collapse" style="overflow:auto; max-height:500px; max-width:400px; border:1px solid #ccc; padding:10px; background-color:#111;">
+        <div id="debug-content" class="collapse debug-collapse tab-content" style="overflow-y:scroll;overflow-x:auto;">
             <div id="general" class="tab-pane fade show active" role="tabpanel">
                 <p><?php echo "SCRIPT_NAME: " . basename($_SERVER["SCRIPT_NAME"]); ?><br>
                 <?php echo "REQUEST_METHOD: " . $_SERVER["REQUEST_METHOD"]; ?></p>
@@ -31,37 +34,47 @@ function include_debug_div() {
                 <p><?php echo "_COOKIE: " . var_export($_COOKIE, true); ?></p>
                 <p><?php echo "_ENV: " . var_export($_ENV, true); ?></p>
             </div>
-            <div id="debug" class="tab-pane fade" role="tabpanel" style="overflow-y:scroll;">
-                <?php
-                foreach (($GLOBALS["debug"] ?? []) as $value) {
-                    echo "$value<br>";
-                }
-                ?>
-            </div>
+            <div id="php-errors" class="tab-pane fade" role="tabpanel"></div>
+            <div id="logger" class="tab-pane fade" role="tabpanel"></div>
         </div>
     </div>
 
     <script>
-        // Make button toggle debug_div visibility:
-        const debugDiv = document.getElementById("debug_div");
-        function restoreDebugDivState() {
-            const isVisible = (sessionStorage.getItem("debug_div_visible") === "true");
-            if (isVisible)
-                debugDiv.classList.remove("d-none");
-            else 
-                debugDiv.classList.add("d-none");
+        function fetch_and_display(url, elementId) {
+            fetch(url)
+                .then(response => response.text())
+                .then(content => {
+                    const formattedContent = content.replace(/\n/g, "<br>");
+                    const element = document.getElementById(elementId);
+                    element.innerHTML = formattedContent;
+                    const debugContent = document.getElementById("debug-content");
+                    debugContent.scrollTop = debugContent.scrollHeight;
+                })
+                .catch(error => {
+                    console.error("Error fetching the file:", error);
+                });
         }
-        function toggleDebugDiv() {
-            debugDiv.classList.toggle("d-none");
-            const isVisible = !debugDiv.classList.contains("d-none");
-            sessionStorage.setItem("debug_div_visible", isVisible);
+        function scrollToBottom(elementId) {
+            const element = document.getElementById(elementId);
+            setTimeout(() => {
+                element.scrollTop = element.scrollHeight;
+            }, 0);
         }
-        function clearDebugDiv() {
-            debugDiv.innerHTML = "";
+        function hideChildren(elementId) {
+            const element = document.getElementById(elementId);
+            for (let i = 0; i < element.children.length; i++)
+                element.children[i].classList.add("d-none");
         }
-        // document.getElementById("debug_div_toggle").addEventListener("click", toggleDebugDiv);
-        // document.getElementById("debug_div_clear").addEventListener("click", clearDebugDiv);
-        // document.addEventListener("DOMContentLoaded", restoreDebugDivState);
+        document.getElementById("button-php-errors").addEventListener("click", () => {
+            // Specify the URL of the file you want to fetch
+            const logFile = "../../logs/php_errors.log";
+            fetch_and_display(logFile, "php-errors");
+        });
+        document.getElementById("button-logger").addEventListener("click", () => {
+            // Specify the URL of the file you want to fetch
+            const logFile = "../../logs/log.txt";
+            fetch_and_display(logFile, "logger");
+        });
     </script>
 
     <?php ob_end_flush();
